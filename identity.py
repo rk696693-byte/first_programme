76a1285bd848b8c7a8d7daa430fa1285fedb0c39
rk696693-byte <rk696693@gmail.com>
2026-06-23 19:34:54 +0530

    update code

---
+++ identity.py
@@ -0,0 +1,11 @@
+a=[1,2,3,4,5]
+b=[1,2,3,4,5]
+c=a
+if a is b:
+    print("they are identical")
+else:
+    print("they are not idencal")
+if a is not c:
+    print("they are not identical")
+else:
+    print("they are identical")
